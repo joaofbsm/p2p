c528256b3b0eef4c9de7285a733d993d239a196e
Joao Martins <joaofbsm@hotmail.com>
2017-07-09 04:55:36 -0300

    Add servent
     - Not working completely yet.

--- client.py
+++ client.py
@@ -11,31 +11,39 @@ __author__ = "João Francisco Martins, Victor Bernardo Jorge and Lorena Cerbino"
 #==================================FUNCTIONS==================================#
 
 def create_msg(key):
-    msg = struct.pack("!H", 1) + key
+    CLIREQ = struct.pack("!H", 1)
+    msg = CLIREQ + key.encode("UTF-8")
+
     return msg
 
 
 def send_msg(sock, msg, ip, port):
     retransmission = False
-
     while True:
-        try:
-            sock.sendto(msg, (ip, port))
-        except socket.timeout:
+        sock.sendto(msg, (ip, port))
+        responses = rcv_msg(sock)
+        if not responses:  # 0 responses received for query
             if not retransmission:
                 retransmission = True
             else:
-                break
-
+                return
+        else:
+            return
 
 def rcv_msg(sock):
+    responses = 0
     while True:
         try:
-            data, addr = sock.receivefrom()
-            print(addr, "response:", data)
+            data, addr = sock.recvfrom(2 + 40 + 1 + 160 + 1)  # Max len = 204
+            responses += 1
+            response = data[2:].decode("UTF-8")
+            print(addr, "response:", response)
         except socket.timeout:
-            print("No more responses.")
-            break
+            if responses > 0:
+                print("No more responses.")
+            else:
+                print("No responses received.")
+            return responses
 
 #====================================MAIN=====================================#
 
@@ -43,19 +51,20 @@ def main(args):
     servent_addr = args[0] 
     servent_addr = servent_addr.split(":")
     servent_ip = servent_addr[0]
-    servent_port = servent_addr[1]
+    servent_port = int(servent_addr[1])
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.settimeout(4)  # 4 seconds timeout
 
-     key = input("Type a key to be retrieved: ")
-     
-     msg = create_msg(key)
+    while True:
+        key = input("Type a key to be retrieved: ")
+         
+        msg = create_msg(key)
 
-     send_msg(sock, msg, servent_ip, servent_port)
+        send_msg(sock, msg, servent_ip, servent_port)
 
-     rcv_msg(sock)
-     
+    sock.close()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- servent.py
+++ servent.py
@@ -10,19 +10,84 @@ __author__ = "João Francisco Martins, Victor Bernardo Jorge and Lorena Cerbino"
 
 # TODO
 # - Maybe use argparse
+# - Change how it is being done to check seq_number
+
+# QUESTIONS
+# - Does python needs to set null char at the end of strings?
+# - Wouldn't it be 202 bytes tops?
+# - Why does the received port number does not match?
 
 #==================================FUNCTIONS==================================#
 
 def parse_keyvalues(keyvalues_file):
-    pass
+    keyvalues = {}
+    with open(keyvalues_file, "r") as f:
+        for line in f:
+            if line[0] != "#":
+                key, value = line.strip().split(maxsplit=1)
+                keyvalues[key] = value
+
+    print(keyvalues)
+
+    return keyvalues
 
 
-def create_msg():
-    pass
+def create_query(msg):
+    QUERY = struct.pack("!H", 2)
+    ttl = struct.pack("!H", msg["ttl"])
+    ip = socket.inet_aton(msg["ip"])
+    port = struct.pack("!H", msg["port"])
+    seq_number = struct.pack("!I", msg["seq_number"])
+    key = msg["key"].encode("UTF-8")
+    query = QUERY + ttl + ip + port + seq_number + key
+    
+    return query
 
 
-def update_msg():
-    pass
+def create_response(key, value):
+    RESPONSE = struct.pack("!H", 3)
+    response = (RESPONSE + key.encode("UTF-8") + b" " + value.encode("UTF-8")
+               + b"\0")
+
+    return response
+
+
+def send_msg(sock, msg, ip, port):
+    sock.sendto(msg, (ip, port))
+
+
+def rcv_msg(sock):
+    data, addr = sock.recvfrom(2 + 2 + 4 + 2 + 4 + 40)  # Max len = 54 bytes
+
+    msg_type = struct.unpack("!H", data[0:2])[0]
+
+    if msg_type == 1:  # Message has type CLIREQ
+        msg = {
+            "type": msg_type,
+            "ip": addr[0],
+            "port": addr[1],
+            "seq_number": -1,  # If new query, seq_number starts at -1
+            "key": data[2:].decode("UTF-8")
+        }
+    else:  # Message has type QUERY
+        msg = {
+            "type": msg_type,
+            "ttl": struct.unpack("!H", data[2:4])[0],
+            "ip": socket.inet_ntoa(data[4:8])[0],
+            "port": struct.unpack("!H", data[8:10])[0], 
+            "seq_number": struct.unpack("!I", data[10:14])[0], 
+            "key": data[14:].decode("UTF-8")
+        }
+
+    return msg
+
+
+def flood_reliably(sock, query, peers, source):
+    print("Flooding peers")
+    for peer in peers:
+        if peer != source:
+            ip, port = peer.split(":")
+            send_msg(sock, query, ip, int(port))
 
 #====================================MAIN=====================================#
 
@@ -37,15 +102,45 @@ def main(args):
 
     readable = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 
                              socket.IPPROTO_UDP)
+    # Prevents "Address already in use" error
     readable.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     readable.bind(("", port))
 
     writable = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 
                              socket.IPPROTO_UDP)
 
+    seen = set()  # Seen queries
     seq_number = 0
-    
 
+    while True:
+        msg = rcv_msg(readable)
+        query = (msg["ip"] + str(msg["port"]) + str(msg["seq_number"]) 
+                 + msg["key"])
+        if query not in seen:
+            seen.add(query)
+            print("New query for key \"", msg["key"], "\" from ", msg["ip"], 
+                  ":", msg["port"], sep="")
+
+            if msg["type"] == 1:
+                msg["ttl"] = 3
+                msg["seq_number"] = seq_number
+            elif msg["type"] == 2:
+                print("Current query has TTL", msg["ttl"])
+                msg["ttl"] -= 1
+
+            if msg["ttl"] > 0:
+                new_query = create_query(msg)
+                flood_reliably(writable, new_query, peers,
+                               (msg["ip"] + ":" + str(msg["port"])))
+                
+            if msg["key"] in keyvalues:
+                response = create_response(msg["key"], keyvalues[msg["key"]])
+                send_msg(writable, response, msg["ip"], msg["port"])
+        else:
+            print("Query already seen.")
+
+    readable.close()
+    writable.close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
